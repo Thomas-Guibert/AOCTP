eb409b440574f970dfb6d5dc86b265d165bd13af
Thomas-Guibert <thomas.guibert0930@gmail.com>
2021-12-26 17:43:47 +0100

    Javadoc CapteurAsync

--- src/aoc/activeobject/gv/CapteurAsync.java
+++ src/aoc/activeobject/gv/CapteurAsync.java
@@ -4,6 +4,11 @@ import java.util.concurrent.Future;
 
 import aoc.activeobject.u.ObserverDeCapteurAsync;
 
+/**
+ * CapteurAsync du pattern active object, retourne la valeur de que contient l'observer de capteur lui aussi asynchrone sous la forme de Future
+ * @author thomas
+ *
+ */
 public interface CapteurAsync {
 	public Future<Integer> getValue(ObserverDeCapteurAsync obs);
 }
