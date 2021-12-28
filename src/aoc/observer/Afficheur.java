57da85f510a05307bf00c346be12adec783e1369
Thomas-Guibert <thomas.guibert0930@gmail.com>
2021-12-28 14:42:30 +0100

    Javadoc Afficheur

--- src/aoc/observer/Afficheur.java
+++ src/aoc/observer/Afficheur.java
@@ -11,29 +11,35 @@ public class Afficheur implements ObserverDeCapteurAsync{
 	private String name;
 	private Canal canal;
 	private ArrayList<Integer> listInt;
-	
+
 	public Afficheur(Canal canal, String name) {
 		listInt = new ArrayList<Integer>();
 		this.name=name;
 		this.canal = canal;
 	}
-	
+
+	/**
+	 * Fonction de mise a joue de l'afficheur, ajoute la valeur du capteur dans la liste 
+	 */
 	@Override
 	public Void update(Capteur s) throws InterruptedException, ExecutionException {
 		listInt.add(this.canal.getValue(this).get());
-		//show();
 		return null;
 	}
-	
+
+	/**
+	 * Fonction d'affichage de l'afficheur 
+	 * @return String le contenu de l'afficheur 
+	 */
 	public String show() {
 		String res = name + " :";
 		if(!listInt.isEmpty()) {
 			res += " " + listInt.get(0);
-        for(int i=1;i<listInt.size();i++) {
-        	res += ", " + listInt.get(i);
-        }
+			for(int i=1;i<listInt.size();i++) {
+				res += ", " + listInt.get(i);
+			}
 		}
-        return res;
+		return res;
 	}
-	
+
 }
