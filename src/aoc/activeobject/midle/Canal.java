529f2ccb2bd129fb6937f3d46ddee857d7517bb0
Thomas-Guibert <thomas.guibert0930@gmail.com>
2021-12-28 14:27:28 +0100

    Javadoc Canal

--- src/aoc/activeobject/midle/Canal.java
+++ src/aoc/activeobject/midle/Canal.java
@@ -12,6 +12,11 @@ import aoc.observer.Afficheur;
 import aoc.observer.Capteur;
 import aoc.observer.ObserverDeCapteur;
 
+/**
+ * Canal 
+ * @author thomas
+ *
+ */
 public class Canal implements ObserverDeCapteur, CapteurAsync{
 
 	Capteur capteur;
@@ -24,6 +29,9 @@ public class Canal implements ObserverDeCapteur, CapteurAsync{
 		this.execService = execService;
 	}
 
+	/**
+	 * Implementation de update de l'interface ObserverDeCapteur qui mette a jour l'afficheur
+	 */
 	@Override
 	public Void update(Capteur c) {
 		execService.schedule(new Callable<Void> () {
@@ -34,6 +42,9 @@ public class Canal implements ObserverDeCapteur, CapteurAsync{
 		return null;
 	}
 
+	/**
+	 * Implementation de getValue de l'interface CapteurAsync qui retourne la valeur du capteur
+	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public Future<Integer> getValue(ObserverDeCapteurAsync obs) {
@@ -44,14 +55,25 @@ public class Canal implements ObserverDeCapteur, CapteurAsync{
 		}, 50, TimeUnit.MILLISECONDS);
 	}
 
+	/**
+	 * 
+	 * @param aff : L'afficheur a associer au canal
+	 */
 	public void setAfficheur(Afficheur aff) {
 		this.afficheur = aff;
 	}
 
+	/**
+	 * Affiche dans un print l'afficheur
+	 */
 	public void showAfficheur() {
 		System.out.println(afficheur.show());
 	}
 
+	/**
+	 * Retour l'afficheur sous forme d'un string
+	 * @return la valeur de l'afficheur
+	 */
 	public String getAfficheur() {
 		return afficheur.show().substring(afficheur.show().indexOf(":")+1, afficheur.show().length());
 	}
